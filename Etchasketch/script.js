0f7ade7c219e859d99560452536203d341a3c6c3
Nols05 <noelliron@gmail.com>
2024-04-29 13:15:27 +0200

    Commit final viendo soluciones

--- Etchasketch/script.js
+++ Etchasketch/script.js
@@ -7,14 +7,20 @@ btn.addEventListener("click", () => {
     grid(n);
 })
 
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) { return; }
+    e.target.style.backgroundColor = "red";
+}
 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 function grid(n) {
     if (n > 100) {
         alert("Máximo 100");
         return;
     }
-
     container.innerHTML = '';
 
     for (let i = 0; i < n * n; i++) {
@@ -22,7 +28,8 @@ function grid(n) {
         div.className = "square";
         div.style.width = `${100 / n}%`
         div.style.height = `${100 / n}%`
-
+        div.addEventListener("mouseover", changeColor)
+        div.addEventListener("mousedown", changeColor)
         container.appendChild(div);
     }
 }
